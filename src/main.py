8e657bfebade5732b1f7e1c0d40e07def134d889
Salvador Desravines <desravines.salvador001@yahoo.com>
2019-09-15 18:45:06 +0000

    modified signup

--- src/main.py
+++ src/main.py
@@ -36,17 +36,18 @@ def sitemap():
     return generate_sitemap(app)
 
 
+
 @app.route('/user', methods=['GET'])
 def get_all_users():
-    contacts_query = Users.query.all()
-    contacts_query = list(map(lambda x: x.serialize(), contacts_query))
-    return jsonify(contacts_query), 200
+    users = Users.query.all()
+    users_query = list(map(lambda x: x.serialize(), users_query))
+    return jsonify(users_query), 200
 
 
 @app.route('/user/<int:user_id>', methods=['DELETE', 'PUT'])
 def delete_user(user_id):
     if request.method == 'DELETE':
-    user1 = User.query.get(user_id)
+        user1 = User.query.get(user_id)
     if user1 is None:
         raise APIException('User not found', status_code=404)
 
@@ -73,21 +74,33 @@ def delete_user(user_id):
     return jsonify(user1.serialize()), 200
 
 
+
+
 @app.route('/signup', methods=['POST'])
 def signup():
     if not request.get_json:
         return jsonify({"msg": "Missing JSON in request"}), 400
 
     params = request.get_json()
+    first_name = params.get('first_name', None)
+    last_name = params.get('last_name', None)
     email = params.get('email', None)
     password = params.get('password', None)
+    # print(first_name)
+    # print(last_name)
     # print(email)
     # print(password)
+    if not first_name:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not last_name:
+        return jsonify({"msg": "Missing username parameter"}), 400
     if not email:
         return jsonify({"msg": "Missing username parameter"}), 400
     if not password:
         return jsonify({"msg": "Missing password parameter"}), 400
 
+    if first_name == '' or last_name == '':
+        return jsonify({"msg": "Need First Name and Last Name"}), 401
     if email == '' or password == '':
         return jsonify({"msg": "Bad username or password"}), 401
 
@@ -96,14 +109,20 @@ def signup():
         return jsonify({"msg": "User Already Exists"}), 405
 
 
-    user1 = Users(email=email, password=password)
+    user1 = Users(first_name=first_name, last_name=last_name, email=email, password=password)
     db.session.add(user1)
     db.session.commit()
-
     return jsonify({"msg": "User created"}), 200
 
-@app.route('/login', methods=['POST'])
+
+
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    users = Users.query.all()
+    users_query = list(map(lambda x: x.serialize(), users_query))
+    return jsonify(users_query), 200
+
     if not request.is_json:
         return jsonify({"msg": "Missing JSON in request"}), 400
 
@@ -112,7 +131,7 @@ def login():
     password = params.get('password', None)
 
     if not email:
-        return jsonify({"msg": "Missing username parameter"}), 400
+        return jsonify({"msg": "Missing email parameter"}), 400
     if not password:
         return jsonify({"msg": "Missing password parameter"}), 400
 
@@ -122,6 +141,7 @@ def login():
     # Identity can be any data that is json serializable
     ret = {'jwt': create_jwt(identity=email)}
     return jsonify(ret), 200
+
 
 # Protect a view with jwt_required, which requires a valid jwt
 # to be present in the headers.
